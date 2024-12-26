54dc7bc5aaafa8e6f7e0df986e9a5359a4b77130
JosemirFarias <josemirfbrito@gmail.com>
2024-12-26 15:56:45 -0300

    Concluindo logica do jogo

--- src/br/com/cod3r/cm/modelo/Tabuleiro.java
+++ src/br/com/cod3r/cm/modelo/Tabuleiro.java
@@ -1,8 +1,9 @@
 package br.com.cod3r.cm.modelo;
 
 import java.util.ArrayList;
-import java.util.Iterator;
+//import java.util.Iterator;
 import java.util.List;
+import java.util.function.Predicate;
 
 public class Tabuleiro {
 	
@@ -21,6 +22,20 @@ public class Tabuleiro {
 		associarVizinhos();
 		sortearMinas();
 	}
+	
+	public void abrir(int linha, int coluna) {
+		campos.parallelStream()
+			.filter(c -> c.getLinha() == linha && c.getColuna()== coluna)
+			.findFirst()
+			.ifPresent(c -> c.abrir());
+	}
+	
+	public void aternarMarcacao(int linha, int coluna) {
+		campos.parallelStream()
+			.filter(c -> c.getLinha() == linha && c.getColuna()== coluna)
+			.findFirst()
+			.ifPresent(c -> c.alternarMarcacao());
+	}
 
 	private void gerarCampos() {
 		for (int linha = 0; linha < linhas; linha++) {
@@ -31,18 +46,47 @@ public class Tabuleiro {
 	}
 
 	private void associarVizinhos() {
-		for (Campo c1 : campos) {
-			for (Campo c2 : campos) {
+		for (Campo c1: campos) {
+			for (Campo c2: campos) {
 				c1.adicionarVizinho(c2);
 			}
 		}
 	}
 
 	private void sortearMinas() {
+		long minasArmadas = 0;
+		Predicate<Campo> minado = c -> c.isMinado();
 		
+		do {
+			minasArmadas = campos.stream().filter(minado).count();
+			int aleatorio = (int) (Math.random() * campos.size());
+			campos.get(aleatorio).minar();
+		} while(minasArmadas < minas);
 	}
 	
+	public boolean objetivoAlcancadoa() {
+		return campos.stream().allMatch(c -> c.objetivoAlcancado());
+	}
 	
+	public void reiniciar() {
+		campos.stream().forEach(c -> c.reiniciar());
+		sortearMinas();
+	}
 	
+	public String toString() {
+		StringBuilder sb = new StringBuilder();
+		
+		int i = 0;
+		for (int l = 0; l < linhas; l++) {
+			for (int c = 0; c < colunas; c++) {
+				sb.append(" ");
+				sb.append(campos.get(i));
+				sb.append(" ");
+				i++;
+			}
+			sb.append("\n");
+		}
+		return sb.toString();
+	}
 
 }
